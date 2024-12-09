46c9fefb7e28bdb742b0ae5ec2aea08b1bc54ac0
manzi osee <85952735+manziosee@users.noreply.github.com>
2024-12-09 11:32:32 +0200

    chore(setup): add initial configuration files

---
+++ src/main/java/com/x_ticket/XTicketApplication.java
@@ -0,0 +1,13 @@
+package com.x_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/x_ticket/XTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
